3bd66d4d47f7a3c218b8f0114db9db39594e0fa4
aahilalwani25 <alwani.aahil25@gmail.com>
2024-10-22 15:01:31 +0500

    add token on signup

--- src/utilities/jwtSign.js
+++ src/utilities/jwtSign.js
@@ -2,7 +2,7 @@ const JWT = require('jsonwebtoken');
 const dotenv = require("dotenv");
 dotenv.config();
 
-async function jwtSignature(data,email){
+async function jwtSignature(data){
     const verificationToken = JWT.sign(data, process.env.JWT_SECRET, {
         expiresIn: "10h",
     });
