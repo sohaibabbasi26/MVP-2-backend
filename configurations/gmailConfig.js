28e41027566cc3ecca518f70700b0e562c3539db
aahilalwani25 <alwani.aahil25@gmail.com>
2024-10-28 14:02:29 +0500

    chore: gmail blocker fix

--- configurations/gmailConfig.js
+++ configurations/gmailConfig.js
@@ -1,18 +1,31 @@
+
 const nodemailer = require("nodemailer");
 const dotenv = require("dotenv");
+const { OAuth2Client } = require("google-auth-library");
 dotenv.config();
 
+const oauth2Client= new OAuth2Client({
+  clientId: process.env.GMAIL_CLIENT_ID,
+  clientSecret: process.env.GMAIL_CLIENT_SECRET,
+})
+
+oauth2Client.setCredentials({
+  refresh_token: process.env.GMAIL_REFRESH_TOKEN,
+});
+
+const accessToken= oauth2Client.getAccessToken();
+
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 587,
+  service: 'gmail',
+  //port: 587,
   auth: {
-      //type: 'OAuth2',
+      type: 'OAuth2',
       user: process.env.GMAIL_EMAIL,
-      pass: process.env.GMAIL_PASS,
-      // clientId: process.env.GMAIL_CLIENT_ID,
-      // clientSecret: process.env.GMAIL_CLIENT_SECRET,
-      // refreshToken: process.env.GMAIL_REFRESH_TOKEN,
-      // accessToken: process.env.GMAIL_ACCESS_TOKEN
+      // pass: process.env.GMAIL_PASS,
+      clientId: process.env.GMAIL_CLIENT_ID,
+      clientSecret: process.env.GMAIL_CLIENT_SECRET,
+      refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+      accessToken: accessToken
   }
 });
 module.exports = transporter;
